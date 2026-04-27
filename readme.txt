2f4bf0b2f4b24ef2310c4babcc41eade8da8377c
Dan Khoo <dankhoo-nassaupos@outlook.com>
2026-04-28 02:04:13 +0500

    commit changes

--- readme.txt
+++ readme.txt
@@ -6,3 +6,8 @@
 /common
   __init__.py
   config.py
+
+
+  -------------------------------
+  
+gcloud run deploy videoai-worker-staging --source . --region us-central1
--- worker/app.py
+++ worker/app.py
@@ -3,10 +3,10 @@ import json
 import logging
 from flask import Flask, request
 
-from handlers.thumbnail import process_thumbnail
-from handlers.render import process_render
-from handlers.optimize import process_optimize
-from handlers.training import process_training
+from handlers.thumbnails import process_thumbnail
+# from handlers.render import process_render
+# from handlers.optimize import process_optimize
+# from handlers.training import process_training
 
 app = Flask(__name__)
 
@@ -36,12 +36,12 @@ def pubsub_handler():
 
         if msg_type == "generate_thumbnail":
             process_thumbnail(params)
-        elif msg_type == "render_clip":
-            process_render(params)
-        elif msg_type == "optimize_video":
-            process_optimize(params)
-        elif msg_type == "retrain_model":
-            process_training(params)
+        # elif msg_type == "render_clip":
+        #     process_render(params)
+        # elif msg_type == "optimize_video":
+        #     process_optimize(params)
+        # elif msg_type == "retrain_model":
+        #     process_training(params)
 
         return ("OK", 200)
 
